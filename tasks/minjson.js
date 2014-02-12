1ceb9ea13ffdb2c3ac92f7a6ac1adc5f72bfeec0
solygen <frank@solygen.de>
2014-02-12 13:21:50 +0100

    start to separate i18n again

--- tasks/minjson.js
+++ tasks/minjson.js
@@ -1,19 +1,27 @@
 module.exports = {
     compile: {
         files: {
-            'data-min/armor.min.json': 'data/armor.json',
-            'data-min/attributes.min.json': 'data/attributes.json',
-            'data-min/classes.min.json': 'data/classes.json',
-            'data-min/combatvalues.min.json': 'data/combatvalues.json',
+            'data-min/deu/armor.min.json': 'data/deu/armor.json',
+            'data-min/deu/attributes.min.json': 'data/deu/attributes.json',
+            'data-min/deu/classes.min.json': 'data/deu/classes.json',
+            'data-min/deu/combatvalues.min.json': 'data/deu/combatvalues.json',
+
+            'data-min/eng/armor.min.json': 'data/eng/armor.json',
+            'data-min/eng/attributes.min.json': 'data/eng/attributes.json',
+            'data-min/eng/classes.min.json': 'data/eng/classes.json',
+            'data-min/eng/combatvalues.min.json': 'data/eng/combatvalues.json',
+
             'data-min/races.min.json': 'data/races.json',
+            'data-min/weapons.min.json': 'data/weapons.json',
             'data-min/class-fighter.min.json': 'data/class-fighter.json',
             'data-min/class-scout.min.json': 'data/class-scout.json',
             'data-min/class-mage.min.json': 'data/class-mage.json',
             'data-min/class-mage-healer.min.json': 'data/class-mage-healer.json',
             'data-min/class-mage-sorcerer.min.json': 'data/class-mage-sorcerer.json',
             'data-min/class-mage-wizard.min.json': 'data/class-mage-wizard.json',
-            'data-min/weapons.min.json': 'data/weapons.json',
-            'dungeonslayers.min.json': ['data/*.json']
+
+            'dungeonslayers-deu.min.json': ['data/deu/**/*.json'],
+            'dungeonslayers-eng.min.json': ['data/eng/**/*.json']
         }
     }
 };
