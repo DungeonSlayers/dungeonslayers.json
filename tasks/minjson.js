12376fc43884a3fc1a9957ffee67ff98423feb4b
solygen <frank@solygen.de>
2014-02-12 13:07:12 +0100

    add talents for basic classes

--- tasks/minjson.js
+++ tasks/minjson.js
@@ -6,6 +6,12 @@ module.exports = {
             'data-min/classes.min.json': 'data/classes.json',
             'data-min/combatvalues.min.json': 'data/combatvalues.json',
             'data-min/races.min.json': 'data/races.json',
+            'data-min/class-fighter.min.json': 'data/class-fighter.json',
+            'data-min/class-scout.min.json': 'data/class-scout.json',
+            'data-min/class-mage.min.json': 'data/class-mage.json',
+            'data-min/class-mage-healer.min.json': 'data/class-mage-healer.json',
+            'data-min/class-mage-sorcerer.min.json': 'data/class-mage-sorcerer.json',
+            'data-min/class-mage-wizard.min.json': 'data/class-mage-wizard.json',
             'data-min/weapons.min.json': 'data/weapons.json',
             'dungeonslayers.min.json': ['data/*.json']
         }
